8a95338703c812562193700dc6177ef4aef11c13
Matt Enyeart <menyeart1@gmail.com>
2023-09-27 09:09:18 -0600

    Add additional capybara tests

--- spec/features/traffic_forecast_feature_spec.rb
+++ spec/features/traffic_forecast_feature_spec.rb
@@ -3,7 +3,10 @@ require "rails_helper"
 RSpec.describe "Traffic forecast feature" do
   describe "As a visitor, when I visit '/traffic_forecast'", :vcr do
       it "I see a header 'traffic forecast' and all the times forecasted for the first resort" do
+
         visit "/"
+        save_and_open_page
+        click_button "Winter Park"
         save_and_open_page
       end
     end
