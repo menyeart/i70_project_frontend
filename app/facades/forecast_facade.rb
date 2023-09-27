d99dc53804ea1a2dd8be4960c948026e36ed9064
Matt Enyeart <menyeart1@gmail.com>
2023-09-27 09:08:18 -0600

    Refactor facade to get one resort as a forecast

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,9 +1,9 @@
 class ForecastFacade
   def initialize
-    @forecast_service  = ForecastService.new.get_forecast_west
+    @@forecast_service ||= ForecastService.new.get_forecast_west
   end
 
-  def create_forecast
-    Forecast.new(@forecast_service)
+  def create_forecast(resort)
+    resort == nil ? forecast = Forecast.new(@@forecast_service) : forecast = Forecast.new(@@forecast_service, resort)
   end
 end
