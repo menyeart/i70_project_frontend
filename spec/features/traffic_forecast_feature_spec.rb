f58c869b049517ecce6048657e0ac6b3e20eabfe
Matt Enyeart <menyeart1@gmail.com>
2023-09-28 09:08:03 -0600

    Refactor test

--- spec/features/traffic_forecast_feature_spec.rb
+++ spec/features/traffic_forecast_feature_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe "Traffic forecast feature" do
       it "I see a header 'traffic forecast' and all the times forecasted for the first resort" do
 
         visit "/"
-        save_and_open_page
         click_button "Winter Park"
         save_and_open_page
+        # expect(page).to have_content("winter Park")
       end
     end
 end
