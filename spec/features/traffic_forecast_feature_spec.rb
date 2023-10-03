bdb83b7d8ff3c190b5c6da83ea19304c478f87a5
Matt Enyeart <menyeart1@gmail.com>
2023-10-03 09:24:36 -0600

    Add additional testing for feature spec

--- spec/features/traffic_forecast_feature_spec.rb
+++ spec/features/traffic_forecast_feature_spec.rb
@@ -3,15 +3,22 @@ require "rails_helper"
 RSpec.describe "Traffic forecast feature" do
   describe "As a visitor, when I visit '/traffic_forecast'", :vcr do
       it "I see a header 'traffic forecast' and all the times forecasted for the first resort" do
+        buttons = ["Loveland", "Winter Park", "Arapahoe Basin", "Keystone", "Breckenridge", "Copper", "Vail", "Beaver Creek", "Copper", "Monarch"]
+        fields = ["Departure Time", "Estimated Travel Time", "Additional Travel Time", "Percent Above Ideal"]
 
         visit "/"
+        
+        expect(page).to have_content("Front Range Freeway Forecast")
+        expect(page).to have_content("Front Range Freeway Forecast")
+
         click_button "Winter Park"
-        save_and_open_page
-        # click_button "Loveland"
-        # save_and_open_page
-        # click_button "Vail"
-        # save_and_open_page
-        # expect(page).to have_content("winter Park")
+
+        buttons.each do |button|
+          expect(page).to have_button(button)
+        end
+        fields.each do |field|
+          expect(page).to have_content(field)
+        end
       end
     end
 end
